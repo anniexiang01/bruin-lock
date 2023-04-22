487390083376da59faf92f31a6a2600596f158e2
BuildTools <unconfigured@null.spigotmc.org>
2023-04-22 01:23:18 -0700

    button changes with status

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { db } from "./firebase";
+import { uid } from "uid";
+import { getDatabase, set, ref, onValue, remove, update } from "firebase/database";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [tempUuid, setTempUuid] = useState("");
+
+  const handleTodoChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  //read
+  useEffect(() => {
+    onValue(ref(db), (snapshot) => {
+      setTodos([]);
+      const data = snapshot.val();
+      if (data !== null) {
+        Object.values(data).map((todo) => {
+          setTodos((oldArray) => [...oldArray, todo]);
+        });
+      }
+    });
+  }, []);
+
+  //write
+  const writeToDatabase = () => {
+    const uuid = uid();
+    set(ref(db, `/${uuid}`), {
+      todo,
+      uuid,
+    });
+
+    setTodo("");
+  };
+
+  //set status
+  const [status, setStatus] = useState(0);
+  const database = getDatabase();
+  const statusRef = ref(database, 'status');
+
+  useEffect(() => {
+    onValue(statusRef, (snapshot) => {
+      const value = snapshot.val();
+      setStatus(value);
+    });
+  }, [statusRef]);
+  
+  const handleButtonClick = () => {
+    const newStatus = status === 0 ? 1 : 0;
+    set(statusRef, newStatus);
+  };
+
+  let buttonText;
+  if (status === 0) {
+    buttonText = 'Click to set status to 1';
+  } else if (status === 1) {
+    buttonText = 'Click to set status to 0';
+  } else {
+    buttonText = 'Invalid status';
+  }
+
+  //update
+  const handleUpdate = (todo) => {
+    setIsEdit(true);
+    setTempUuid(todo.uuid);
+    setTodo(todo.todo);
+  };
+
+  const handleSubmitChange = () => {
+    update(ref(db, `/${tempUuid}`), {
+      todo,
+      uuid: tempUuid,
+    });
+
+    setTodo("");
+    setIsEdit(false);
+  };
+
+  //delete
+  const handleDelete = (todo) => {
+    remove(ref(db, `/${todo.uuid}`));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={todo} onChange={handleTodoChange} />
+      {isEdit ? (
+        <>
+          <button onClick={handleSubmitChange}>Submit Change</button>
+          <button
+            onClick={() => {
+              setIsEdit(false);
+              setTodo("");
+            }}
+          >
+            X
+          </button>
+        </>
+      ) : (
+        <button onClick={writeToDatabase}>submit</button>
+      )}
+      <div>
+        <p>Current status is: {status}</p>
+        <button onClick={handleButtonClick}>{buttonText}</button>
+      </div>
+      {todos.map((todo) => (
+        <>
+          <h1>{todo.todo}</h1>
+          <button onClick={() => handleUpdate(todo)}>update</button>
+          <button onClick={() => handleDelete(todo)}>delete</button>
+        </>
+      ))}
     </div>
   );
 }
 
 export default App;
+
+// npm install firebase
+// npm install uid
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2ZuJc9EMZJw7tZsAl83pLeOifhd9WDKI",
+  authDomain: "bruinlocks.firebaseapp.com",
+  databaseURL: "https://bruinlocks-default-rtdb.firebaseio.com",
+  projectId: "bruinlocks",
+  storageBucket: "bruinlocks.appspot.com",
+  messagingSenderId: "681344432351",
+  appId: "1:681344432351:web:4464dc2c277b0ec809116a",
+  measurementId: "G-S2FQ85HNXW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
